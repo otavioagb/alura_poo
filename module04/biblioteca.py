03b7267786e0ba5b54a98a04cdf790c013cc3127
Otávio Brito <o.augusto.brito@gmail.com>
2025-01-01 22:26:20 -0300

    Alura Module 04 and modules refactoring

--- module04/biblioteca.py
+++
@@ -1,9 +0,0 @@
-from livro import Livro
-
-book_one = Livro('1984', 'George Orwell', 1949)
-book_two = Livro('O Poder Do Hábito', 'Charles Duhhig', 2012)
-# for book in Livro.books:
-#     print(book)
-book_one.land()
-available = Livro.check_availability(2012)
-print(available)
